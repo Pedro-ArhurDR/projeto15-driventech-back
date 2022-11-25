f21b82a0d2ef07dc260bddf1f01471021eb90d8f
bis <bis@pop-os.localdomain>
2022-11-24 22:25:45 -0300

    feat: created database, but contains errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import { json } from "body-parser";
-import express from "express";
+
+import express, {json} from "express";
 import cors from "cors"
+
 
 const app = express();
 app.use(json());
 app.use(cors())
+
+
 
 app.listen(5000, "Conectado na porta 5000");
